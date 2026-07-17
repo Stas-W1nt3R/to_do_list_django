0cf0a40574a3380599c4717e133890ef1060876a
Stanislav Kondratiev <stanislaw.comza@gmail.com>
2026-07-17 22:58:29 +0300

    day 2 admin panel

--- TodoList/TodoList/urls.py
+++ TodoList/TodoList/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.contrib import admin
 from django.urls import path
 from mysite import views
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("",views.MainPage, name='MainPage'),
     path('registration/', views.registration, name='registration'),
     path('enter/', views.enter, name='enter'),
+
 ]
--- TodoList/mysite/models.py
+++ TodoList/mysite/models.py
@@ -3,25 +3,31 @@ from django.contrib.auth.models import User
 
 
 class Blog(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=20)
-    description = models.TextField()
-    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь",related_name="blogs")
+    title = models.CharField(max_length=20, verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Описание блога")
+    date = models.DateField(verbose_name="Дата создания")
 
     def __str__(self):
         return self.title
 
     class Meta:
         db_table = 'blogs'
+        verbose_name='Блог'
+        verbose_name_plural = 'Блоги'
+        ordering = ['-date']
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    comment = models.TextField()
-    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name="Пользователь",related_name="user_comments")
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE,verbose_name="Блог",related_name="blog_comments")
+    comment = models.TextField(verbose_name="Комментарий")
+    date = models.DateField(verbose_name="Дата создания")
 
     def __str__(self):
-        return self.comment
+        return f"Комментарий от {self.user.username}"
 
     class Meta:
         db_table = 'comments'
+        ordering = ['-date']
+        verbose_name='Комментарий'
+        verbose_name_plural = 'Комментарии'
